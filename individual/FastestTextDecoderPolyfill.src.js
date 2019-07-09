0e2a64fb7f23bb1270ee89db32a98a76a0dc4447
Jack Giffin <wowzeryest@gmail.com>
2019-07-09 13:48:00 -0400

    Update FastestTextDecoderPolyfill.src.js

--- individual/FastestTextDecoderPolyfill.src.js
+++ individual/FastestTextDecoderPolyfill.src.js
@@ -8,33 +8,28 @@
   var NativeUint8Array = window.Uint8Array;
   var patchedU8Array = NativeUint8Array || Array;
   var ArrayBufferString = Object_prototype_toString.call((window.ArrayBuffer || patchedU8Array).prototype);
-  function decoderReplacer(nonAsciiChars){
-    // make the UTF string into a binary UTF-8 encoded string
-    var point = nonAsciiChars.charCodeAt(0)|0;
-    if (point >= 0xD800 && point <= 0xDBFF) {
-      var nextcode = nonAsciiChars.charCodeAt(1)|0;
-      if (nextcode !== nextcode) // NaN because string is 1 code point long
-        return fromCharCode(0xef/*11101111*/, 0xbf/*10111111*/, 0xbd/*10111101*/);
-      // https://mathiasbynens.be/notes/javascript-encoding#surrogate-formulae
-      if (nextcode >= 0xDC00 && nextcode <= 0xDFFF) {
-        point = ((point - 0xD800)<<10) + nextcode - 0xDC00 + 0x10000|0;
-        if (point > 0xffff)
-          return fromCharCode(
-            (0x1e/*0b11110*/<<3) | (point>>>18),
-            (0x2/*0b10*/<<6) | ((point>>>12)&0x3f/*0b00111111*/),
-            (0x2/*0b10*/<<6) | ((point>>>6)&0x3f/*0b00111111*/),
-            (0x2/*0b10*/<<6) | (point&0x3f/*0b00111111*/)
-          );
-      } else return fromCharCode(0xef, 0xbf, 0xbd);
+  function decoderReplacer(encoded){
+    var codePoint = encoded.charCodeAt(0) << 24;
+    var leadingOnes = clz32(~codePoint)|0;
+    var endPos = 0, stringLen = encoded.length|0;
+    var result = "";
+    if (leadingOnes < 5 && stringLen >= leadingOnes) {
+      codePoint = (codePoint<<leadingOnes)>>>(24+leadingOnes);
+      for (endPos = 1; endPos < leadingOnes; endPos=endPos+1|0)
+        codePoint = (codePoint<<6) | (encoded.charCodeAt(endPos)&0x3f/*0b00111111*/);
+      if (codePoint <= 0xFFFF) { // BMP code point
+        result += fromCharCode(codePoint);
+      } else if (codePoint <= 0x10FFFF) {
+        // https://mathiasbynens.be/notes/javascript-encoding#surrogate-formulae
+        codePoint = codePoint - 0x10000|0;
+        result += fromCharCode(
+          (codePoint >> 10) + 0xD800|0,  // highSurrogate
+          (codePoint & 0x3ff) + 0xDC00|0 // lowSurrogate
+        );
+      } else endPos = 0; // to fill it in with INVALIDs
     }
-    if (point <= 0x007f) return nonAsciiChars;
-    else if (point <= 0x07ff) {
-      return fromCharCode((0x6<<5)|(point>>>6), (0x2<<6)|(point&0x3f));
-    } else return fromCharCode(
-      (0xe/*0b1110*/<<4) | (point>>>12),
-      (0x2/*0b10*/<<6) | ((point>>>6)&0x3f/*0b00111111*/),
-      (0x2/*0b10*/<<6) | (point&0x3f/*0b00111111*/)
-    );
+    for (; endPos < stringLen; endPos=endPos+1|0) result += "\ufffd"; // replacement character
+    return result;
   }
   function TextDecoder(){};
   TextDecoder.prototype.decode = function(inputArrayOrBuffer){
@@ -46,7 +41,7 @@
     for (var index=0,len=inputAs8.length|0; index<len; index=index+32768|0)
       resultingString += fromCharCode.apply(0, inputAs8[NativeUint8Array ? "slice" : "subarray"](index,index+32768|0));
 
-    return resultingString.replace(/[\x80-\uD7ff\uDC00-\uFFFF]|[\uD800-\uDBFF][\uDC00-\uDFFF]?/g, decoderReplacer);
+    return resultingString.replace(/[\xc0-\xff][\x80-\xbf]*/g, decoderReplacer);
   }
   if (!window["TextDecoder"]) window["TextDecoder"] = TextDecoder;
 })(typeof global == "" + void 0 ? typeof self == "" + void 0 ? this : self : global);
