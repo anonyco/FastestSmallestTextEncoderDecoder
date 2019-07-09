0c7d2919221cabab1401989b09293a855ceb653d
Jack Giffin <wowzeryest@gmail.com>
2019-07-09 11:28:16 -0400

    Update hexWorker.js

--- gh-pages/hexWorker.js
+++ gh-pages/hexWorker.js
@@ -34,7 +34,10 @@ onmessage= (function() {
         currentTextBuffer = null; // reset it
         
         let resultingString = "";
-        for (let i=0; i<len; i=i+1|0) resultingString += (i === 0 ? "" : " ") + (inputAsU8Array[i]|0).toString(16);
+        for (let i=0; i<len; i=i+1|0) resultingString += (
+            (i === 0 ? (inputAsU8Array[i]|0) < 16 ? "0" : "" : (inputAsU8Array[i]|0) < 16 ? " 0" : " ") +
+            (inputAsU8Array[i]|0).toString(16)
+        );
         postMessage({
             "type": "hex",
             "value": resultingString
